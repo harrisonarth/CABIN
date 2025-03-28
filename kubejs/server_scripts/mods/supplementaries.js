33248a38feea2c4b1505b6a51942de9f0b531e3c
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2025-03-28 13:38:00 -0500

    Better Lumisene recipes

--- kubejs/server_scripts/mods/supplementaries.js
+++ kubejs/server_scripts/mods/supplementaries.js
@@ -1,5 +1,23 @@
 if (Platform.isLoaded("supplementaries")) {
     ServerEvents.recipes(event => {
+        //Lumisene
+        event.custom({
+            "type": "tconstruct:melting",
+            "ingredient": {"item": "minecraft:glow_berries"},
+            "result": {
+                "fluid": "supplementaries:lumisene",
+                "amount": 125
+            },
+            "temperature": 200,
+            "time": 6
+        }).id("kubejs:smeltery/melting/lumisene")
+    
+        event.recipes.thermal.crucible(Fluid.of("supplementaries:lumisene", 125), "minecraft:glow_berries", 0, 1000)
+    
+        event.recipes.create.filling("supplementaries:lumisene_bottle", ["minecraft:glass_bottle", Fluid.of("supplementaries:lumisene", 250).toJson()])
+        event.recipes.create.emptying([Fluid.of("supplementaries:lumisene", 250), "minecraft:glass_bottle"], "supplementaries:lumisene_bottle")
+
+        //Timber Frame
         event.remove({ id:"supplementaries:timber_frame" })
         donutCraft(event, Item.of("supplementaries:timber_frame", 2), "minecraft:air", "#forge:rods/wooden")
 
--- kubejs/server_scripts/recipes/illusonaryblocks.js
+++ kubejs/server_scripts/recipes/illusonaryblocks.js
@@ -1,17 +1,5 @@
 ServerEvents.recipes(event => {
 
-    event.custom({
-        "type": "tconstruct:melting",
-        "ingredient": {"item": "minecraft:glow_berries"},
-        "result": {
-            "fluid": "supplementaries:lumisene",
-            "amount": 100
-        },
-        "temperature": 200,
-        "time": 6
-    }).id("kubejs:smeltery/melting/lumisene")
-
-
     let illusion = (input) => {
         const output = `kubejs:trial_${input.replace(/^minecraft:|^trials:/, "")}`;
         event.custom({
