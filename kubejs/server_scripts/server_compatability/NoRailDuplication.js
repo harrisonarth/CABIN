f22724595ac5d8298acc50b29bae53c720162f72
meishiffx <57701493+meishiffx@users.noreply.github.com>
2025-04-14 19:34:18 +0800

    Update NoRailDuplication.js
    ' → “

--- kubejs/server_scripts/server_compatability/NoRailDuplication.js
+++ kubejs/server_scripts/server_compatability/NoRailDuplication.js
@@ -1,5 +1,5 @@
 ServerEvents.recipes(e => {
-    e.remove({id:'tconstruct:smeltery/melting/metal/iron/nugget_3'}),
-    e.remove({id:'tconstruct:smeltery/melting/metal/iron/ingot_1'}),
-    e.remove({id:'tconstruct:smeltery/melting/metal/gold/powered_rail'})
+    e.remove({id:"tconstruct:smeltery/melting/metal/iron/nugget_3"}),
+    e.remove({id:"tconstruct:smeltery/melting/metal/iron/ingot_1"}),
+    e.remove({id:"tconstruct:smeltery/melting/metal/gold/powered_rail"})
 })
